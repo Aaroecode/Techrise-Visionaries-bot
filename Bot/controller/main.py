940ed7b21de63c0d48f9de8a227e4183308ec6ab
Aaroecode <Aaroecode@gmail.com>
2023-10-21 02:10:43 +0530

    Initialized FIles

---
+++ Bot/controller/main.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+from dotenv import load_dotenv
+import discord, os
+
+load_dotenv()
+
+intents = discord.Intents.all()
+
+Bot = commands.Bot(command_prefix="t!", intents=intents)
+
+
+
+
+
---
+++ run.py
@@ -0,0 +1,7 @@
+from Bot.controller.main import Bot
+from dotenv import load_dotenv
+import os
+env_path = os.getcwd()
+load_dotenv(env_path=env_path)
+if __name__ == '__main__':
+    Bot.run(os.getenv("TOKEN"))
